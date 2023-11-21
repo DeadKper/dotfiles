c2297cd9855758cb4f475577daa4f4f77028be9a
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2023-11-20 20:31:56 -0700

    fix vim func

--- .config/fish/functions/vim.fish
+++ .config/fish/functions/vim.fish
@@ -1,5 +1,5 @@
 function vim --argument file
-	if test -z "$file"
+	if test -z "$file" # no file given, just open vim
 		if type -q nvim
 			command nvim
 		else if type -q vim
@@ -9,18 +9,28 @@ function vim --argument file
 		end
 		return 0
 	end
-	set -f pwd "$PWD"
-	if test -n (path extension "$file")
-		set -f folder (dirname "$file")
-	else
+	
+	set -f pwd "$PWD" # save current dir
+
+	if string match -r '.*/$' -- "$file" # ends witch '/' so it's a folder
 		set -f folder "$file"
 		set -f file "."
+	else
+		set -f folder (dirname "$file")
+		set -f file (basename "$file")
 	end
-	if test ! -d "$folder"
+
+	if test ! -d "$folder" # create folder if it doesn't exist
+		set -l folder_copy "$folder"
+		set -f folders
+		while test ! -d "$folder_copy"
+			set -a folders (basename "$folder_copy") # save created folders
+			set folder_copy (dirname "$folder_copy")
+		end
 		mkdir -p "$folder"
 	end
+
 	cd "$folder"
-	set -f empty . ..
 	if type -q nvim
 		command nvim -- "$file"
 	else if type -q vim
@@ -28,9 +38,13 @@ function vim --argument file
 	else
 		command nano -- "$file"
 	end
-	cd "$pwd"
-	while is_empty_dir "$folder"; and test ! "$folder" = "."
-		rm -rf "$folder"
-		set -f folder (dirname "$folder")
+
+	cd ..
+	while test (count $folders) -gt 0; and is_empty_dir "$folders[1]" # remove empty folders if created
+		rm -rf "$folders[1]"
+		set -e folders[1]
+		cd ..
 	end
+
+	cd "$pwd" # cd back to original dir
 end
