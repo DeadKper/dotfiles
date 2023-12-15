c07c1f2af6aeb0dc59564bb65efafea2b0c4ab66
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2023-12-14 20:16:14 -0700

    change name for commands of tmux init

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -2,17 +2,13 @@ if not set -q TMUX; and status is-interactive
     set -x TMUX_HOME 'home'
     
     if not tmux has-session -t "$TMUX_HOME" &> /dev/null
-        set -f __init_tmux 1
-
         tmux new-session -s "$TMUX_HOME" -c "$HOME" -d
-        sleep 0.1
-    end
 
-    if test "$__init_tmux" = '1'
         source "$HOME/.config/tmux-init-conf.fish"
 
         set -f cnt (seq (math (count $tmux_window_names)))
-        echo $cnt
+
+        sleep 0.1 # wait for session to be created
 
         for i in $cnt
             if test "$i" != '1'
@@ -31,6 +27,7 @@ if not set -q TMUX; and status is-interactive
         end
 
         tmux select-window -t "$TMUX_HOME:{end}"
+        sleep 0.2 # wait for fish to load
 
         for i in $cnt
             tmux send-keys -t "$TMUX_HOME:$i" "$tmux_window_cmds[$i]" Enter
