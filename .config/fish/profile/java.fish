4d3557326854fbb7e38cd81fc3035be5cfed1bb4
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-02-24 13:23:07 -0700

    remove unneded files

--- .config/fish/profile/java.fish
+++
@@ -1,2 +0,0 @@
-set -x JAVA_HOME (dirname (readlink -f /usr/bin/java))
-fish_add_path $JAVA_HOME
--- .config/fish/profile/rust.fish
+++
@@ -1,6 +0,0 @@
-set -x RUSTUP_HOME "$HOME/.local/share/rust/rustup"
-set -x CARGO_HOME "$HOME/.local/share/rust/cargo"
-
-if test -d "$CARGO_HOME/bin"
-  fish_add_path "$CARGO_HOME/bin"
-end
