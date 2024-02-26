0e6a5fb8c220d375c5a031437ab58a57d9f44725
Missael Gastelum <missaga@proton.me>
2024-02-26 17:09:45 -0600

    return correct status number

--- .config/fish/functions/dnf.fish
+++ .config/fish/functions/dnf.fish
@@ -1,8 +1,11 @@
 function dnf --wraps dnf
-    command dnf $argv 2>| read -d '\n' -a output
-    if echo $output | grep -F 'Error: This command has to be run with superuser privileges' &> /dev/null
+    command dnf $argv 2> ~/.cache/dnf-err
+    set -l stat $status
+    if cat ~/.cache/dnf-err | grep -F 'Error: This command has to be run with superuser privileges' &> /dev/null
         command sudo dnf $argv
     else
-        printf '%s\n' $output
+        cat ~/.cache/dnf-err
+        rm ~/.cache/dnf-err
+        return $stat
     end
 end
