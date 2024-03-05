99df386f4054774421fe0df11e0ea2bd104dec19
Missael Gastelum <missaga@proton.me>
2024-03-05 16:09:12 -0600

    update neovim functions

--- .config/fish/functions/vimdiff.fish
+++ .config/fish/functions/vimdiff.fish
@@ -3,5 +3,5 @@ function vimdiff
         echo vimdiff requires 2 files to compare >&2
         return 1
     end
-    nvim +'vsplit | bnext | windo diffthis | windo set noma | windo set nofoldenable' $argv
+    nvim +'vsplit | bnext | windo diffthis | windo set noma | windo set nofoldenable | map q :qa!<cr>' $argv
 end
