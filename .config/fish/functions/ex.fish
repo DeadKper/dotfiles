fd54401db4c812bb5a4c81364e522b6cddf2712e
Missael Gastelum <missaga@proton.me>
2024-03-07 17:39:39 -0600

    import from work branch

--- .config/fish/functions/ex.fish
+++ .config/fish/functions/ex.fish
@@ -1,43 +1,45 @@
 function ex --description "Expand or extract bundled & compressed files" --argument file dir
-  set --local ext (echo $file | awk -F. '{print $NF}')
+    set --local ext (echo $file | awk -F. '{print $NF}')
 
-  if test (echo $file | awk -F. '{print $(NF-1)}') = tar
-    set ext tar.{$ext}
-  end
+    if test (echo $file | awk -F. '{print $(NF-1)}') = tar
+        set ext tar.{$ext}
+    end
 
-  if test -n "$dir"
-    mkdir "$dir"
+    if test -z "$dir"
+        set dir (echo $file | sed 's/.'$ext'$//')
+    end
+
+    mkdir -p "$dir"
+
+    set -l pwd (pwd)
     mv "$file" "$dir"
     cd "$dir"
-  end
 
-  switch $ext
-  case tar.gz or tgz
-    tar -zxvf $file
-  case tar.bz2 or tbz2
-    tar -jxvf $file
-  case tar.xz
-    tar -Jxvf $file
-  case tar  # non-compressed, just bundled
-    tar -xvf $file
-  case gz
-    gunzip $file
-  case bz2
-    bunzip2 $file
-  case rar
-    unrar x $file
-  case zip or crx
-    unzip $file
-  case Z
-    uncompress $file
-  case 7z
-    7z x $file
-  case '*'
-    echo "Unknown compressed extension '."$ext"'"
-  end
+    switch $ext
+        case tar.gz or tgz
+            tar -zxvf $file
+        case tar.bz2 or tbz2
+            tar -jxvf $file
+        case tar.xz
+            tar -Jxvf $file
+        case tar # non-compressed, just bundled
+            tar -xvf $file
+        case gz
+            gunzip $file
+        case bz2
+            bunzip2 $file
+        case rar
+            unrar x $file
+        case zip or crx
+            unzip $file
+        case Z
+            uncompress $file
+        case 7z
+            7z x $file
+        case '*'
+            echo "Unknown compressed extension '."$ext"'"
+    end
 
-  if test -n "$dir"
-    mv "$file" ".."
-    cd ".."
-  end
+    mv "$file" "$pwd"
+    cd "$pwd"
 end
--- .config/fish/functions/vim.fish
+++ .config/fish/functions/vim.fish
@@ -12,52 +12,10 @@ function vim --argument-names file
     end
 
     # check if it's a wslpath
-    if type -q wslpath; and wslpath "$file" &> /dev/null
+    if type -q wslpath; and wslpath "$file" &>/dev/null
         set -f file (wslpath "$file")
-        set -f valid 'y'
-    else if test -n "$(echo $file | grep -E '^(\.?|[^\0/-][^\0/]+)((/[^\0/]+)+/?|/?)$')"
-        set -f valid 'y'
     end
 
-    if test (count $argv) -ne 1 -o -z "$valid"
-        # more than 1 arg or args is not a path, just open vim
-        command $vim $argv && return 0; return 1
-    end
-    
-    set -f pwd "$PWD" # save current dir
-
-    # ends with '/' so it's a folder
-    if string match -q -r '.*/$' -- "$file"
-        set -f folder "$file"
-        set -f file "."
-    else
-        set -f folder (dirname "$file")
-        set -f file (basename "$file")
-    end
-
-    # create folder if it doesn't exist
-    if test ! -d "$folder"
-        set -l folder_copy "$folder"
-        set -f folders
-        while test ! -d "$folder_copy"
-            set -a folders (basename "$folder_copy") # save created folders
-            set folder_copy (dirname "$folder_copy")
-        end
-        mkdir -p "$folder"
-    end
-    
-    cd "$folder"
-    set -l error 1
-    command $vim -- "$file" && set error 0
-
-    cd ..
-    # remove empty folders if created
-    while test (count $folders) -gt 0; and is_empty_dir "$folders[1]"
-        rm -rf "$folders[1]"
-        set -e folders[1]
-        cd ..
-    end
-
-    cd "$pwd" # cd back to original dir
-    return $error
+    command $vim $argv
+    return $status
 end
