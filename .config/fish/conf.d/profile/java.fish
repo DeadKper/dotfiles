5b8e2f5d11b2d5cd5d20a26e4fb54bd1622064f0
Missael <missaga@proton.me>
2024-03-15 22:34:51 -0700

    set java only if it's installed

--- .config/fish/conf.d/profile/java.fish
+++ .config/fish/conf.d/profile/java.fish
@@ -1,2 +1,4 @@
-set -x JAVA_HOME (dirname (dirname (readlink -f /usr/bin/java)))
-add-path PATH $JAVA_HOME
+if type -q java
+    set -x JAVA_HOME (dirname (dirname (readlink -f /usr/bin/java)))
+    add-path PATH $JAVA_HOME
+end
