d087c7a9b6bd73eba8b52ffa85dadad073e4365c
Missael Gastelum <missaga@proton.me>
2024-04-01 14:03:38 -0600

    make telnet wrapper more useful

--- .config/fish/functions/telnet.fish
+++ .config/fish/functions/telnet.fish
@@ -1,6 +1,23 @@
-function telnet --argument ip port timeout
-    if test -z "$timeout"
-        set timeout 20s
+if type -q telnet.exe
+    function telnet --argument ip port time
+        if test (count $argv) -lt 2
+            echo 'usage: telnet IP PORT <TIME>'
+            return 1
+        end
+        if test -z "$time"
+            timeout 5s telnet.exe "$ip" "$port"
+            if test "$status" = 124
+                timeout 25s telnet.exe "$ip" "$port"
+                if test "$status" = 124
+                    echo "Connection timed out 2 times, try again specifying connection timeout or 0 to disable it"
+                end
+            end
+        else if test "$time" -eq 0
+            telnet.exe "$ip" "$port"
+        else
+            timeout {$time}s telnet.exe "$ip" "$port"
+            echo "Connection timed out, try again increasing connection timeout or 0 to disable it"
+        end
     end
-    timeout "$timeout" telnet.exe "$ip" "$port"
 end
+
