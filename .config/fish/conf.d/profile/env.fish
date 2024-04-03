f23867fd29794ea84b63e58ecef2a5e0bb4cc00b
DeadKper <missaga@proton.me>
2024-04-02 20:40:26 -0700

    don't set keymap on wayland

--- .config/fish/conf.d/profile/env.fish
+++ .config/fish/conf.d/profile/env.fish
@@ -2,16 +2,16 @@ set -x fish_greeting
 set -x EDITOR nvim
 set -x VISUAL nvim
 
-if type -q setxkbmap
-	set -x XKB_DEFAULT_LAYOUT (setxkbmap -query | grep layout | sed -r "s/^layout:\t* *(.*)/\1/g")
-	set -x XKB_DEFAULT_VARIANT (setxkbmap -query | grep variant | sed -r "s/^variant:\t* *(.*)/\1/g")
+if type -q setxkbmap; and test -z "$WAYLAND_DISPLAY"
+    set -x XKB_DEFAULT_LAYOUT (setxkbmap -query | grep layout | sed -r "s/^layout:\t* *(.*)/\1/g")
+    set -x XKB_DEFAULT_VARIANT (setxkbmap -query | grep variant | sed -r "s/^variant:\t* *(.*)/\1/g")
 end
 
 
 if test -d ~/.surrealdb
-	add-path PATH ~/.surrealdb
+    add-path PATH ~/.surrealdb
 end
 
 if test -z "$JDTLS_JVM_ARGS" -a -f "$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar"
-	set -x JDTLS_JVM_ARGS "-javaagent:$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar"
+    set -x JDTLS_JVM_ARGS "-javaagent:$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar"
 end
