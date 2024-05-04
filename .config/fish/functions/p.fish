17ae02592826b4bdedced1afa5bbb1a3ad28e5ec
DeadKper <missaga@proton.me>
2024-05-04 11:06:30 -0700

    rename ps to p

--- .config/fish/functions/p.fish
+++ .config/fish/functions/p.fish
@@ -1,4 +1,4 @@
-function ps
+function p
     set -l positional
     getopts $argv | while read -l key value
         switch $key
@@ -32,10 +32,10 @@ function ps
                     end
                 end
             case h help
-                printf '%s\n' 'usage: ps [flags] [query...]'
+                printf '%s\n' 'usage: p [flags] [query...]'
                 printf '%s\t%s\n' '-t --type' 'select type to open proyect, \'s\' for a new tmux session, \'w\' for a new tmux window and \'t\' (default) for current terminal'
                 printf '%s\t%s\n' '-c --cmd' 'command to execute on the selected proyect folder, defaults to \'nvim .\' if installed, or vim/vi'
-                printf '%s\t%s\n' '--conf  ' 'config file to use, defaults to \'~/.config/deadkper/proyect-selector.fish\', config file should \'set -f cmd <command with args>\' if not already set, and \'set -f proyects <array of proyect folders>\''
+                printf '%s\t%s\n' '--conf  ' 'config file to use, defaults to \'~/.config/proyect-selector.fish\', config file should \'set -f cmd <command with args>\' if not already set, and \'set -f proyects <array of proyect folders>\''
                 printf '%s\t%s\n' '-h --help' 'prints help message'
                 printf '%s\t%s\n' '[query...]' 'querys to match with \'grep -i\' on the proyects'
                 return 0
@@ -76,8 +76,8 @@ function ps
     end
 
     if not set -q TMUX
-        if test -f "$HOME/.config/deadkper/tmux.fish"
-            source "$HOME/.config/deadkper/tmux.fish"
+        if test -f "$HOME/.config/tmux-init-conf.fish"
+            source "$HOME/.config/tmux-init-conf.fish"
         end
 
         if test -n "$tmux_session_home_name"
@@ -86,7 +86,6 @@ function ps
             set -fx TMUX_HOME home
         end
     end
-
 
     if not set -q type
         set -f type t
@@ -97,6 +96,7 @@ function ps
         echo "'s' new tmux session" 1>&2
         return 1
     end
+
     if not set -q cmd
         if type -q nvim
             set -f cmd nvim
@@ -116,18 +116,14 @@ function ps
         end
     end
 
-    if not set -q XDG_CACHE_HOME
-        set -f XDG_CACHE_HOME "$HOME/.cache"
-    end
-
-    printf '%s\n' $proyects | string replace -r "^$HOME" '~' | sort >"$XDG_CACHE_HOME/ps_proyects"
+    printf '%s\n' $proyects | string replace -r "^$HOME" '~' | sort >/tmp/__p_proyects
 
     set -f curr_session (tmux display-message -p '#S')
 
     switch $type
         case t
             if test -z "$selected"
-                set -f selected (cat "$XDG_CACHE_HOME/ps_proyects" | fzf | string replace -r '^~' "$HOME")
+                set -f selected (cat "/tmp/__p_proyects" | sk | string replace -r '^~' "$HOME")
             end
             if test -n "$selected"
                 cd "$selected"
@@ -135,18 +131,20 @@ function ps
             end
         case s w
             if test -z "$selected"
-                tmux new-window -t "$curr_session:0" -n sessionizer "cat '$XDG_CACHE_HOME/ps_proyects' | fzf | string replace -r '^~' '$HOME' > '$XDG_CACHE_HOME/ps_selected'"
+                tmux new-window -t "$curr_session:0" -n sessionizer "cat '/tmp/__p_proyects' | sk | string replace -r '^~' '$HOME' > '/tmp/__p_selected'"
                 while tmux list-windows | rg -e '^0: sessionizer' &>/dev/null
                     sleep 0.1
                 end
-                set -f selected (cat "$XDG_CACHE_HOME/ps_selected")
-                rm "$XDG_CACHE_HOME/ps_selected"
+                set -f selected (cat "/tmp/__p_selected")
+                rm /tmp/__p_*
             end
             if test "$type" = s
                 set -f session (basename "$selected")
                 if not tmux has-session -t "$session" 2>/dev/null
                     tmux new-session -d -s "$session" -c "$selected"
-                    sleep 0.3 # Wait for fish to load
+                    sleep 0.1 # Wait for session to be created
+                    tmux switch-client -t "$curr_session"
+                    sleep 0.4 # Wait for fish to load
                     tmux send-keys -t "$session:" "$cmd" Enter
                 end
                 tmux switch-client -t "$session"
@@ -158,7 +156,7 @@ function ps
                         set -f window "$curr_session:$i"
                     end
                 end
-                sleep 0.3 # Wait for fish to load
+                sleep 0.5 # Wait for fish to load
                 tmux send-keys -t "$window" "$cmd" Enter
                 tmux select-window -t "$window"
             end
