7196864390fcaaffa976b17431981c2f26e6a227
Missael Gastelum <missaga@proton.me>
2024-06-17 11:22:08 -0600

    fix submodules commands

--- .config/fish/functions/sync.fish
+++ .config/fish/functions/sync.fish
@@ -1,8 +1,8 @@
 function sync --description 'alias sync=yadm pull --all --recurse-submodules'
-  if yadm submodule status | grep --quiet '^-'
-    yadm submodule update --init --recursive
-  end
-  yadm pull --all --recurse-submodules
-  git -C "$XDG_CONFIG_HOME/nvim" checkout main &> /dev/null
-  git -C "$XDG_CONFIG_HOME/nvim" pull
+    if yadm -C "$HOME" submodule status | grep --quiet '^-'
+        yadm -C "$HOME" submodule update --init --recursive
+    end
+    yadm pull --all --recurse-submodules
+    git -C "$XDG_CONFIG_HOME/nvim" checkout main &>/dev/null
+    git -C "$XDG_CONFIG_HOME/nvim" pull
 end
