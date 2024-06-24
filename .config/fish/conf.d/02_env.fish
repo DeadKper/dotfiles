adfe3d6a248a44b78f29c048c96ea9fadc95394a
Missael Gastelum <missaga@proton.me>
2024-06-24 11:10:33 -0600

    rename env and add manpath

--- .config/fish/conf.d/02_env.fish
+++ .config/fish/conf.d/02_env.fish
@@ -6,3 +6,7 @@ if type -q setxkbmap; and test -z "$WAYLAND_DISPLAY"
     set -x XKB_DEFAULT_LAYOUT (setxkbmap -query | grep layout | sed -r "s/^layout:\t* *(.*)/\1/g")
     set -x XKB_DEFAULT_VARIANT (setxkbmap -query | grep variant | sed -r "s/^variant:\t* *(.*)/\1/g")
 end
+
+if type -q manpath; and not set -q MANPATH
+    set -x --path MANPATH $(manpath)
+end
