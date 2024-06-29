1b3cd94d4cfe61a44cca86136aa249bff03822bc
DeadKper <missaga@proton.me>
2024-06-29 11:07:59 -0700

    make cliptype a script

--- .config/fish/functions/cliptype.fish
+++
@@ -1,9 +0,0 @@
-function cliptype --argument sleepTime
-    test -z "$sleepTime" && set sleepTime 3
-    sleep $sleepTime
-    if test -z "$WAYLAND_DISPLAY"
-        xdotool type "$(xclip -o -selection clipboard)"
-    else
-        ydotool type "$(wl-paste | sed -e 'y/|\'¿´+}ñ{-°!"&\/()=?¡¨*]Ñ[;:_/`-=[]\\\\;\'\/~!@^&*()_+{}|:"<>?/')"
-    end
-end
