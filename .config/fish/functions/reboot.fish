e7a8a6302f29bc071dcb1c5e17171459f934da3e
Missael Gastelum <missaga@proton.me>
2024-07-13 15:28:59 -0700

    remove shutdown funcs

--- .config/fish/functions/reboot.fish
+++
@@ -1,9 +0,0 @@
-function reboot
-    if test -z "$argv"
-        if not test (qdbus org.kde.ksmserver /KSMServer logout 0 1 2 &> /dev/null)
-            sudo shutdown -r now
-        end
-    else
-        command reboot $argv
-    end
-end
--- .config/fish/functions/shutdown.fish
+++
@@ -1,9 +0,0 @@
-function shutdown
-    if test -z "$argv"
-        if not test (qdbus org.kde.ksmserver /KSMServer logout 0 2 1 &> /dev/null)
-            sudo command shutdown now
-        end
-    else
-        command shutdown $argv
-    end
-end
