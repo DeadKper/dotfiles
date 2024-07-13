0ba6eff57280bed1d142afde57d223eec54ff7ed
Missael Gastelum <missaga@proton.me>
2024-07-13 15:28:51 -0700

    simpler copy

--- .config/fish/functions/copy.fish
+++ .config/fish/functions/copy.fish
@@ -1,12 +1,3 @@
 function copy --wraps='rsync -ahAHSX --info=progress2' --description 'alias copy=rsync -ahAHSX --info=progress2'
-    if test (count $argv) = 2 -a -d "$argv[1]" -a \( ! -e "$argv[2]" -o -d "$argv[2]" \)
-        if test ! -e "$argv[2]"
-            cp -auxv $argv
-        end
-        if count $argv/{.*,*} >/dev/null
-            rsync -ahAHSX --info=progress2 $argv[1]/{.*,*} "$argv[2]"
-        end
-    else
-        rsync -ahAHSX --info=progress2 $argv
-    end
+    rsync -ahAHSX --info=progress2 $argv
 end
