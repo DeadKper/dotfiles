97c71543f5de66ea73670bee1cb9b2626bd83423
Missael Gastelum <missaga@proton.me>
2024-07-13 20:16:38 -0700

    add sudo function and remove symlinks

---
+++ .config/fish/functions/sudo.fish
@@ -0,0 +1,8 @@
+function sudo --wraps="sudo" --description 'alias sudo=sudo'
+    if command -q doas; and not command -q sudo
+        set -f sudo doas
+    else
+        set -f sudo sudo
+    end
+    command $sudo XDG_DATA_HOME="$XDG_DATA_HOME" XDG_CONFIG_HOME="$XDG_CONFIG_HOME" TMUX="$TMUX" fish -c "$(printf '\'%s\' ' $argv)"
+end
