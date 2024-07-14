30f810962925ef8e4a4fdc179329984052cf02c0
Missael Gastelum <missaga@proton.me>
2024-07-14 09:42:49 -0700

    chante ydotool socket path

--- .config/fish/conf.d/02_env.fish
+++ .config/fish/conf.d/02_env.fish
@@ -1,7 +1,7 @@
 set -x fish_greeting
 set -x EDITOR nvim
 set -x VISUAL nvim
-set -x YDOTOOL_SOCKET "$HOME/.local/run/ydotool"
+set -x YDOTOOL_SOCKET "$HOME/.local/state/ydotool.socket"
 
 if type -q manpath; and not set -q MANPATH
     set -x --path MANPATH $(manpath)
