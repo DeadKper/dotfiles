6e651523bd17b26bb626a1b78e24780e58b4a25c
Missael Gastelum <missaga@proton.me>
2024-07-13 20:31:29 -0700

    only set sudo command if needed

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,10 +1,10 @@
 function sudo --wraps="sudo" --description 'alias sudo=sudo'
-    if command -q doas; and not command -q sudo
-        set -f sudo doas
-    else
-        set -f sudo sudo
-    end
     if test "$USER" != root
+        if command -q doas; and not command -q sudo
+            set -f sudo doas
+        else
+            set -f sudo sudo
+        end
         command $sudo VISUAL="$VISUAL" EDITOR="$EDITOR" XDG_DATA_HOME="$XDG_DATA_HOME" XDG_CONFIG_HOME="$XDG_CONFIG_HOME" TMUX="$TMUX" PATH="$PATH" fish -c "$(printf '\'%s\' ' $argv)"
     else
         command $argv
