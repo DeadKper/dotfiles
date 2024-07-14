7cf9a042098a13e4cbadaf89987bb459fec0ee4c
Missael Gastelum <missaga@proton.me>
2024-07-13 20:07:32 -0700

    remove setxkbmap

--- .config/fish/conf.d/02_env.fish
+++ .config/fish/conf.d/02_env.fish
@@ -3,11 +3,6 @@ set -x EDITOR nvim
 set -x VISUAL nvim
 set -x YDOTOOL_SOCKET "$HOME/.local/run/ydotool"
 
-if type -q setxkbmap; and test -z "$WAYLAND_DISPLAY"
-    set -x XKB_DEFAULT_LAYOUT (setxkbmap -query | grep layout | sed -r "s/^layout:\t* *(.*)/\1/g")
-    set -x XKB_DEFAULT_VARIANT (setxkbmap -query | grep variant | sed -r "s/^variant:\t* *(.*)/\1/g")
-end
-
 if type -q manpath; and not set -q MANPATH
     set -x --path MANPATH $(manpath)
 end
