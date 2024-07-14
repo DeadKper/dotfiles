ab80379cc2a4354ad74aa5db46de87edb281b0d9
Missael Gastelum <missaga@proton.me>
2024-07-13 17:50:41 -0700

    add more autosudo to zyp wrapper

--- .config/fish/functions/zyp.fish
+++ .config/fish/functions/zyp.fish
@@ -1,6 +1,6 @@
 if command -q zypper
     function zyp --wraps=zypper --description 'alias zyp=zypper'
-        if echo "$argv[1]" | rg -q '^(in|install|rm|remove)$'
+        if echo "$argv[1]" | rg -q '^(in|install|up|update|rm|remove|ref|refresh|ar|addrepo|rr|removerepo|nr|renamerepo|mr|modifyrepo)$'
             sudo zypper $argv
         else
             zypper $argv
