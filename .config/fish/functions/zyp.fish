7fd101a87eebbe169b3f1983fc78e4c107f51301
Missael Gastelum <missaga@proton.me>
2024-07-15 20:56:01 -0700

    more autosudo

--- .config/fish/functions/zyp.fish
+++ .config/fish/functions/zyp.fish
@@ -1,6 +1,6 @@
 if command -q zypper
     function zyp --wraps=zypper --description 'alias zyp=zypper'
-        if echo "$argv[1]" | rg -q '^(in|install|up|update|dup|dist\-upgrade|rm|remove|ref|refresh|ar|addrepo|rr|removerepo|nr|renamerepo|mr|modifyrepo)$'
+        if echo "$argv[1]" | rg -q '^(in|install|up|update|dup|dist\-upgrade|rm|remove|ref|refresh|ar|addrepo|rr|removerepo|nr|renamerepo|mr|modifyrepo|ve|verify)$'
             sudo zypper $argv
         else
             zypper $argv
