8f265eef556e3e4fa0258709ca5538cc7adb4fde
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2024-08-13 10:07:25 -0700

    fix sudo inside distrobox

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,21 +1,21 @@
 function sudo --wraps="sudo" --description 'alias sudo=sudo'
-    set -f env y
+    set -f flags '-sE'
 
     for arg in $argv
         if echo $arg | grep -q '^-'
             if echo $arg | grep '^--' | grep -qE 'preserve‐env'
-                set -f env n
+                set -f flags
             else if echo $arg | grep -qE 'k|K|i'
-                set -f env n
+                set -f flags
             end
         else
             break
         end
     end
 
-    if test "$env" = y
-        command sudo -sE $argv
+    if type distrobox-host-exec &>/dev/null && echo "$(which $argv[1])" | grep -qF "/container/$(basename "$argv[1]")"
+        distrobox-host-exec sudo $flags $argv
     else
-        command sudo $argv
+        command sudo $flags $argv
     end
 end
