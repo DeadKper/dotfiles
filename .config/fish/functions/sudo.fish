518dd269123bbd80f410cae1d30554cb8b679192
Missael Gastelum <missaga@proton.me>
2024-08-18 15:46:41 -0700

    fix PATH not being preserved correctly

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,5 +1,5 @@
 function sudo --wraps="sudo" --description 'alias sudo=sudo'
-    set -f flags '-sE'
+    set -f flags --preserve-env=PATH -sE
 
     for arg in $argv
         if echo $arg | grep -q '^-'
@@ -16,6 +16,7 @@ function sudo --wraps="sudo" --description 'alias sudo=sudo'
     if type distrobox-host-exec &>/dev/null && echo "$(which $argv[1])" | grep -qF "/container/$(basename "$argv[1]")"
         distrobox-host-exec sudo $flags $argv
     else
+        echo command sudo $flags $argv
         command sudo $flags $argv
     end
 end
