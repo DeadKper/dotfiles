b640ccc789d5c8af843ef2ea6c7ec201ea641b70
Missael Gastelum <missaga@proton.me>
2024-08-18 21:47:24 -0700

    fix sudo -i

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -2,10 +2,8 @@ function sudo --wraps="sudo" --description 'alias sudo=sudo'
     set -f flags --preserve-env=PATH -sE
 
     for arg in $argv
-        if echo $arg | grep -q '^-'
-            if echo $arg | grep '^--' | grep -qE 'preserve‐env'
-                set -f flags
-            else if echo $arg | grep -qE 'k|K|i'
+        if echo -- $arg | grep -q '^-'
+            if echo -- $arg | grep -qE 'k|K|i'
                 set -f flags
             end
         else
@@ -13,7 +11,7 @@ function sudo --wraps="sudo" --description 'alias sudo=sudo'
         end
     end
 
-    if type distrobox-host-exec &>/dev/null && echo "$(which $argv[1])" | grep -qF "/container/$(basename "$argv[1]")"
+    if type distrobox-host-exec &>/dev/null && echo "$(which $argv[1] 2>/dev/null)" | grep -qF "/container/$(basename "$argv[1]" 2>/dev/null)"
         distrobox-host-exec sudo $flags $argv
     else
         command sudo $flags $argv
