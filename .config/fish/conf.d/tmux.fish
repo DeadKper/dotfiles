a1a6c09fbfecb4e04ed30f50802730dc42f5a895
Missael Gastelum <missaga@proton.me>
2024-08-22 20:48:33 -0700

    only attach home session the first time

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -2,8 +2,9 @@ if status is-interactive; and not set -q TMUX_LOADED; and type -q tmux; and not 
     set -x TMUX_LOADED
     set TMUX_HOME home
 
-    if tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null
-        tmux detach-client -s "$TMUX_HOME" -a
+    if tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null; and test -z "$(tmux ls 2>/dev/null | grep -F "$TMUX_HOME:" | grep -vF '(attached)')"
+        exit 0
+        # tmux detach-client -s "$TMUX_HOME" -a
     end
 
     if not tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null
