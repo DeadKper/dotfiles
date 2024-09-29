121eabd96937e0bbdcba7b2bfdf6ac52d56bc92d
Missael Gastelum <missaga@proton.me>
2024-09-29 16:32:39 -0700

    still use tmux xd

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -3,13 +3,13 @@ if status is-interactive; and not set -q TMUX_LOADED; and type -q tmux; and not 
     set TMUX_HOME home
 
     if tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null; and test -z "$(tmux ls 2>/dev/null | grep -F "$TMUX_HOME:" | grep -vF '(attached)')"
-        exit 0
         # tmux detach-client -s "$TMUX_HOME" -a
-    end
-
-    if not tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null
-        tmux attach-session -t "$TMUX_HOME" -c "$HOME"
+        return
     else
-        tmux
+        if not tmux info &>/dev/null; and tmux has-session -t "$TMUX_HOME" &>/dev/null
+            tmux attach-session -t "$TMUX_HOME" -c "$HOME"
+        else
+            tmux
+        end
     end
 end
