22263ccdb84ea25b4d42de5b875c31477c933330
Missael Gastelum <missaga@proton.me>
2024-10-16 16:17:11 -0600

    update fish config

---
+++ .config/fish/conf.d/ansible.fish
@@ -0,0 +1,18 @@
+if type -q ansible
+    abbr apl ansible-playbook
+    abbr apv ansible-playbook --extra-vars
+    abbr apa ansible-playbook asd.yaml -i
+
+    abbr agl ansible-galaxy
+    abbr agi ansible-galaxy init
+
+    abbr aed ansible-edit
+
+    abbr arn ansible-run -m
+    abbr arc ansible-run -m shell -a
+    abbr arx ansible-run -m script -a
+
+    abbr alg ansible-logs
+
+    abbr atp ansible-template
+end
--- .config/fish/conf.d/zy_container.fish
+++ .config/fish/conf.d/zy_container.fish
@@ -1,7 +1,7 @@
 if type -q wslpath
     if not set -q WSL_PATH
         set -x --path PATH $PATH
-        set -x --path WSL_PATH $(/mnt/c/Windows/system32/cmd.exe /c echo %PATH% 2>/dev/null | sd ';' '\n' | xargs -d '\n' -n 1 wslpath)
+        set -x --path WSL_PATH $(/mnt/c/Windows/system32/cmd.exe /c echo %PATH% 2>/dev/null | xargs -r -d ';' -n 1 wslpath | grep '\S')
         for wpath in $WSL_PATH
             add-path -a PATH "$wpath"
         end
