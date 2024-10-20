b279899f433b7bf86ada7d4a879d803da21187c6
Missael Gastelum <missaga@proton.me>
2024-10-19 19:20:24 -0700

    use nvim as manpager

--- .config/fish/conf.d/02_env.fish
+++ .config/fish/conf.d/02_env.fish
@@ -1,6 +1,7 @@
 set -x fish_greeting
 set -x EDITOR nvim
 set -x VISUAL nvim
+set -x MANPAGER 'nvim +Man!'
 set -x YDOTOOL_SOCKET "$HOME/.local/state/ydotool.socket"
 
 if type -q manpath; and not set -q MANPATH
