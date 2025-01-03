9ac1e8a6710194f928a7a31389a8ef349015dd7d
Missael Gastelum <missaga@proton.me>
2025-01-03 14:02:27 -0700

    make tmux session as final step

--- .config/zsh/conf.d/zz_env.zsh
+++ .config/zsh/conf.d/zz_env.zsh
@@ -1,1 +1,11 @@
-export ZSH_LOGIN=true
+export TMUX_HOME=home
+
+if [[ -o interactive ]] && which tmux &>/dev/null && ! tmux info &>/dev/null && tmux ls | awk '/^'"${TMUX_HOME}"':/ { if ($NF == "(attached)") { exit 1 }}'; then
+    if tmux has-session -t "$TMUX_HOME" &>/dev/null; then
+        tmux attach-session -t "$TMUX_HOME" -c "$HOME"
+    else
+        tmux new-session -s "$TMUX_HOME" -c "$HOME"
+    fi
+fi
+
+test -z "${ZSH_LOGIN}" && export ZSH_LOGIN=true
