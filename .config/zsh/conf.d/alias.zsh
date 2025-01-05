ba450a73ac0ac97e3cabf9210111bd3e570674e4
Missael Gastelum <missaga@proton.me>
2025-01-05 16:50:25 -0700

    fix fzf command and pkill/pgrep alias

--- .config/zsh/conf.d/alias.zsh
+++ .config/zsh/conf.d/alias.zsh
@@ -1,7 +1,7 @@
-which nvim &>/dev/null && alias vim=nvim
-
 # preserve env and aliases
-alias sudo='sudo --preserve-env=PATH -E '
+alias sudo="sudo --preserve-env=PATH -E "
+
+which nvim &>/dev/null && alias vim=nvim
 
 if which lsd &>/dev/null; then
     alias ls=lsd
@@ -16,10 +16,5 @@ alias ll="ls -l"
 alias la="ls -A"
 alias lla="ls -lA"
 
-if which fzf &>/dev/null; then
-    alias cf='__path="$(find . -mindepth 1 -type d | sed "s,^\./,," | sort -rV | fzf)"; test -n "$__path" && cd "$__path"'
-    alias zf='__path="$(find . -mindepth 1 -type d | sed "s,^\./,," | sort -rV | fzf)"; test -n "$__path" && z "$__path"'
-fi
-
-alias pkill="pkill -i"
-alias pgrep="pgrep -i"
+alias pkill="pkill -i "
+alias pgrep="pgrep -i "
--- .config/zsh/conf.d/fzf.zsh
+++ .config/zsh/conf.d/fzf.zsh
@@ -1,8 +1,8 @@
 if [[ -o interactive ]] && which fzf &>/dev/null; then
     if which fd &>/dev/null; then
-        export FZF_DEFAULT_COMMAND='fd -u -t d --min-depth 1 | sort -r'
+        export FZF_DEFAULT_COMMAND='fd -t d --min-depth 1'
     else
-        export FZF_DEFAULT_COMMAND='find -mindepth 1 -type d | sort -r'
+        export FZF_DEFAULT_COMMAND='find -L . -mindepth 1 -type d \( -path "*/.*" -prune -o -not -name ".*" \) 2>/dev/null | grep -v "/\." | sed "s,^\./,,"'
     fi
     eval "$(fzf --zsh)"
 fi
