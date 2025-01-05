66e54651fb95da751fac89c758c685a4191c363a
Missael Gastelum <missaga@proton.me>
2025-01-05 15:41:20 -0700

    use array for path vars

--- .config/zsh/conf.d/01_xdg.zsh
+++ .config/zsh/conf.d/01_xdg.zsh
@@ -5,22 +5,18 @@ if [[ -o login ]]; then
     test -z "${XDG_DATA_HOME+1}"   && export XDG_DATA_HOME=~/.local/share
     test -z "${XDG_STATE_HOME+1}"  && export XDG_STATE_HOME=~/.local/state
 
-    if test -z "${XDG_STATE_HOME+1}"; then
-        export XDG_DATA_DIRS="/usr/local/share:/usr/share"
-        if test -d /usr/share/kde-settings/kde-profile/default/share; then
-            export XDG_DATA_DIRS="/usr/share/kde-settings/kde-profile/default/share:${XDG_DATA_DIRS}"
-        fi
+    typeset -TUx XDG_DATA_DIRS xdg_data_dirs
+    xdg_data_dirs+=("/usr/local/share:/usr/share")
+    if test -d /usr/share/kde-settings/kde-profile/default/share; then
+        xdg_data_dirs=("/usr/share/kde-settings/kde-profile/default/share" "${xdg_data_dirs[@]}")
     fi
-    typeset -U path XDG_DATA_DIRS
 
-    if test -z "${XDG_CONFIG_DIRS+1}"; then
-        export XDG_CONFIG_DIRS="/etc/xdg"
-        if test -d ~/.config/kdedefaults; then
-            export XDG_CONFIG_DIRS="${HOME}/.config/kdedefaults:${XDG_CONFIG_DIRS}"
-        fi
-        if test -d /usr/share/kde-settings/kde-profile/default/xdg; then
-            export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:/usr/share/kde-settings/kde-profile/default/xdg"
-        fi
+    typeset -TUx XDG_CONFIG_DIRS xdg_config_dirs
+    xdg_config_dirs+=("/etc/xdg")
+    if test -d ~/.config/kdedefaults; then
+        xdg_config_dirs=("${HOME}/.config/kdedefaults" "${xdg_config_dirs[@]}")
     fi
-    typeset -U path XDG_CONFIG_DIRS
+    if test -d /usr/share/kde-settings/kde-profile/default/xdg; then
+        xdg_config_dirs+=("/usr/share/kde-settings/kde-profile/default/xdg")
+    fi
 fi
