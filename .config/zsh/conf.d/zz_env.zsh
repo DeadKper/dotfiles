58891d3c660c89369e189905cce52cfc5c2bbf4a
Missael Gastelum <missaga@proton.me>
2025-01-05 18:59:55 -0700

    fix tmux error message

--- .config/zsh/conf.d/zz_env.zsh
+++ .config/zsh/conf.d/zz_env.zsh
@@ -2,7 +2,7 @@ export TMUX_HOME=home
 
 path=("$HOME/.local/scripts" "${XDG_DATA_HOME:=$HOME/.local/share}/bin" "$HOME/.local/bin" "${path[@]}")
 
-if [[ -o interactive ]] && which tmux &>/dev/null && ! tmux info &>/dev/null && tmux ls | awk '/^'"${TMUX_HOME}"':/ { if ($NF == "(attached)") { exit 1 }}'; then
+if [[ -o interactive ]] && which tmux &>/dev/null && ! tmux info &>/dev/null && tmux ls 2>/dev/null | awk '/^'"${TMUX_HOME}"':/ { if ($NF == "(attached)") { exit 1 }}'; then
     if tmux has-session -t "$TMUX_HOME" &>/dev/null; then
         tmux attach-session -t "$TMUX_HOME" -c "$HOME"
     else
