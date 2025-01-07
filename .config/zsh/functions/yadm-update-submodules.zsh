dac6e3537834959f7d6137e01f9f71341d0927f6
Missael Gastelum <missaga@proton.me>
2025-01-07 10:46:46 -0700

    fix updater logic for non-tagged repos

--- .config/zsh/functions/yadm-update-submodules.zsh
+++ .config/zsh/functions/yadm-update-submodules.zsh
@@ -1,13 +1,33 @@
 function yadm-update-submodules() {
     local submodules=("${(@f)$(yadm -C ~ config --file .gitmodules --get-regexp path | awk '{print $2}')}")
     local pad=$(( $(printf '%s\n' "${submodules[@]}" | awk '{print length}' | sort -rn | head -1)+5 ))
+
+    trap 'unset -f git' EXIT
+
+    function git() {
+        env git -C "$HOME/$submodule" "$@"
+    }
+
     for submodule in "${submodules[@]}"; do
-        local commit="$(git -C "$HOME/$submodule" ls-remote -qt --sort=committerdate | tail -n 1 | cut -f1)"
-        if test -n "$commit"; then
-            git -C "$HOME/$submodule" fetch
-            git -C "$HOME/$submodule" checkout "$commit" |& xargs -r -d \\n -I {} printf "%-${pad}s %s\n" "[~/$submodule]:" '{}'
-        else
-            printf "%-${pad}s %s\n" "[~/$submodule]:" 'No tags were found'
-        fi
+        {
+            git remote update
+            if git show-ref --quiet --tags; then
+                local commit="$(git ls-remote --quiet --tags --sort=committerdate \
+                    | tail -n 1 \
+                    | cut -f1)"
+
+                git fetch
+                git checkout "$commit"
+            else
+                if test -n "$(git symbolic-ref --quiet --short HEAD)"; then
+                    git pull --rebase
+                else
+                    git checkout "$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')"
+                    if git status -uno |& grep '^Your branch is behind'; then
+                        git pull --rebase
+                    fi
+                fi
+            fi
+        } |& xargs -r -d \\n -I {} printf "%-${pad}s %s\n" "[~/$submodule]:" '{}'
     done
 }
