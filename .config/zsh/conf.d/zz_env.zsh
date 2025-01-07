871568def0f0a8db650ae493f981aa2e2dedb4a6
Missael Gastelum <missaga@proton.me>
2025-01-07 07:45:40 -0700

    fix tmux not autostarting

--- .config/zsh/conf.d/zz_env.zsh
+++ .config/zsh/conf.d/zz_env.zsh
@@ -2,6 +2,6 @@ export TMUX_HOME=home
 
 path=("$HOME/.local/scripts" "${XDG_DATA_HOME:=$HOME/.local/share}/bin" "$HOME/.local/bin" "${path[@]}")
 
-if [[ -o interactive ]] && [[ -o login ]] && which tmux &>/dev/null && ! tmux info &>/dev/null && tmux ls 2>/dev/null | awk '/^'"${TMUX_HOME}"':/ { if ($NF == "(attached)") { exit 1 }}'; then
+if [[ -o interactive ]] && which tmux &>/dev/null && ! tmux info &>/dev/null && tmux ls 2>/dev/null | awk '/^'"${TMUX_HOME}"':/ { if ($NF == "(attached)") { exit 1 }}'; then
     tmux new-session -s "$TMUX_HOME" -c "$HOME" &>/dev/null || tmux attach-session -t "$TMUX_HOME" -c "$HOME"
 fi
