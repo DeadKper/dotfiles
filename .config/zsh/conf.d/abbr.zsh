851ddf9df292e5dab8bdf25480ed72346b489a43
Missael Gastelum <missaga@proton.me>
2025-01-14 10:30:04 -0700

    better abbr logic

--- .config/zsh/conf.d/abbr.zsh
+++ .config/zsh/conf.d/abbr.zsh
@@ -1,42 +1,42 @@
 [[ -o interactive ]] || return
 
 setopt extendedglob
-typeset -Ag word_abbreviations
+typeset -Ag instant_abbreviations
 typeset -Ag space_abbreviations
 
-word_abbreviations=(
-    '...'       '../..'
+instant_abbreviations=(
+    '...'   '../..'
 )
 
 space_abbreviations=(
-    'mkdir'     'mkdir -p'
-    'visudo'    'sudo visudo'
-    'edit'      'sudoedit'
-    'pkill'     'pkill -i'
-    'pgrep'     'pgrep -i'
+    mkdir   'mkdir -p'
+    visudo  'sudo visudo'
+    edit    'sudoedit'
+    pkill   'pkill -i'
+    pgrep   'pgrep -i'
 )
 
 if which yadm &>/dev/null; then
     space_abbreviations+=(
-        'yad'   'yadm add'
-        'ycm'   'yadm commit -m "<CURSOR>"'
-        'yca'   'yadm commit -am "<CURSOR>"'
-        'yst'   'yadm status'
-        'ydf'   'yadm diff'
-        'ypl'   'yadm pull --rebase'
-        'yps'   'yadm push'
+        yad 'yadm add'
+        ycm 'yadm commit -m "<CURSOR>"'
+        yca 'yadm commit -am "<CURSOR>"'
+        yst 'yadm status'
+        ydf 'yadm diff'
+        ypl 'yadm pull --rebase'
+        yps 'yadm push'
     )
 fi
 
 if which git &>/dev/null; then
     space_abbreviations+=(
-        'gad'   'git add'
-        'gcm'   'git commit -m "<CURSOR>"'
-        'gca'   'git commit -am "<CURSOR>"'
-        'gst'   'git status'
-        'gdf'   'git diff'
-        'gpl'   'git pull --rebase'
-        'gps'   'git push'
+        gad 'git add'
+        gcm 'git commit -m "<CURSOR>"'
+        gca 'git commit -am "<CURSOR>"'
+        gst 'git status'
+        gdf 'git diff'
+        gpl 'git pull --rebase'
+        gps 'git push'
     )
 fi
 
@@ -61,54 +61,47 @@ if which ansible &>/dev/null; then
     )
 fi
 
-function self-insert() {
-    zle .self-insert
+function expand-abbreviation() {
     setopt extendedglob
-    [[ "${#RBUFFER}" == 0 || -n "${RBUFFER[1]%%[a-zA-Z0-9${WORDCHARS/-/\\-}]}" ]] || return
-    local MATCH
-    LBUFFER="${LBUFFER%%(#m)[a-zA-Z0-9${WORDCHARS/-/\\-}]#}"
-    local abbreviation="${word_abbreviations[$MATCH]}"
-    LBUFFER+="${abbreviation:-$MATCH}"
-    if [[ "${abbreviation}" =~ "<CURSOR>" ]]; then
-        RBUFFER="${LBUFFER[(ws:<CURSOR>:)2]}$RBUFFER"
-        LBUFFER="${LBUFFER[(ws:<CURSOR>:)1]}"
+    test "${3}" = instant && zle "${1}"
+    if [[ "${#RBUFFER}" != 0 && -z "${RBUFFER[1]%%[a-zA-Z0-9${2}]}" ]]; then
+        test "${3}" != instant && zle "${1}"
+        return
     fi
-}
-
-function accept-line() {
-    setopt extendedglob
-    [[ "${#RBUFFER}" == 0 || -n "${RBUFFER[1]%%[a-zA-Z0-9_]}" ]] || { zle .accept-line; return }
     local MATCH
-    LBUFFER="${LBUFFER%%(#m)[a-zA-Z0-9_]#}"
-    local abbreviation="${space_abbreviations[$MATCH]}"
-    LBUFFER+="${abbreviation:-$MATCH}"
-    if [[ "${abbreviation}" =~ "<CURSOR>" ]]; then
-        RBUFFER="${LBUFFER[(ws:<CURSOR>:)2]}$RBUFFER"
-        LBUFFER="${LBUFFER[(ws:<CURSOR>:)1]}"
+    LBUFFER="${LBUFFER%%(#m)[a-zA-Z0-9${2}]#}"
+    if test "${3}" = instant; then
+        local abbreviation="${instant_abbreviations[$MATCH]}"
+    else
+        local abbreviation="${space_abbreviations[$MATCH]}"
     fi
-    zle .accept-line
-}
-
-function abbreviation-expand() {
-    setopt extendedglob
-    [[ "${#RBUFFER}" == 0 || -n "${RBUFFER[1]%%[a-zA-Z0-9_]}" ]] || { zle .self-insert; return }
-    local MATCH
-    LBUFFER="${LBUFFER%%(#m)[a-zA-Z0-9_]#}"
-    local abbreviation="${space_abbreviations[$MATCH]}"
     LBUFFER+="${abbreviation:-$MATCH}"
     if [[ "${abbreviation}" =~ "<CURSOR>" ]]; then
         RBUFFER="${LBUFFER[(ws:<CURSOR>:)2]}$RBUFFER"
         LBUFFER="${LBUFFER[(ws:<CURSOR>:)1]}"
     else
-        zle .self-insert
+        test "${3}" = space && zle "${1}"
     fi
+    test "${3}" = line && zle "${1}"
+}
+
+function self-insert() {
+    expand-abbreviation .self-insert "${WORDCHARS/-/\\-}" instant
+}
+
+function accept-line() {
+    expand-abbreviation .accept-line _ line
+}
+
+function space-expansion() {
+    expand-abbreviation .self-insert _ space
 }
 
 zle -N self-insert
 zle -N accept-line
-zle -N abbreviation-expand
+zle -N space-expansion
 
-bindkey " " abbreviation-expand
+bindkey " " space-expansion
 bindkey "^x " .self-insert
 bindkey "^x^M" .accept-line
 bindkey -M isearch " " .self-insert
