6d386dfcdf4bb6e7268dc64887875caecb6ec94c
Missael Gastelum <missaga@proton.me>
2025-01-19 14:33:50 -0700

    don't check for login, always set vars

--- .config/zsh/conf.d/npm.zsh
+++ .config/zsh/conf.d/npm.zsh
@@ -1,16 +1,14 @@
-if [[ -o login ]]; then
-    NPM_PACKAGES="${XDG_DATA_HOME:-$HOME/.local/share}/npm-packages"
+NPM_PACKAGES="${XDG_DATA_HOME:-$HOME/.local/share}/npm-packages"
 
-    if test ! -f "$HOME/.npmrc"; then
-        echo "prefix = $NPM_PACKAGES" | sed "s,$HOME,~,g" >~/.npmrc
-    fi
-
-    path=("$NPM_PACKAGES/bin" "${path[@]}")
-    manpath=("$NPM_PACKAGES/share/man" "${manpath[@]}")
-    export NODE_PATH="$NPM_PACKAGES/lib/node_modules"
-    if test ! -d "$NPM_PACKAGES"; then
-        mkdir -p "$NPM_PACKAGES"
-    fi
-
-    unset NPM_PACKAGES
+if test ! -f "$HOME/.npmrc"; then
+    echo "prefix = $NPM_PACKAGES" | sed "s,$HOME,~,g" >~/.npmrc
 fi
+
+path=("$NPM_PACKAGES/bin" "${path[@]}")
+manpath=("$NPM_PACKAGES/share/man" "${manpath[@]}")
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules"
+if test ! -d "$NPM_PACKAGES"; then
+    mkdir -p "$NPM_PACKAGES"
+fi
+
+unset NPM_PACKAGES
--- .config/zsh/conf.d/rust.zsh
+++ .config/zsh/conf.d/rust.zsh
@@ -1,5 +1,3 @@
-if [[ -o login ]]; then
-    export RUSTUP_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rust/rustup"
-    export CARGO_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rust/cargo"
-    path=("$CARGO_HOME/bin" "${path[@]}")
-fi
+export RUSTUP_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rust/rustup"
+export CARGO_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rust/cargo"
+path=("$CARGO_HOME/bin" "${path[@]}")
