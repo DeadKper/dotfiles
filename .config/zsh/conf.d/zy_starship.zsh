d2305be17ef8ced463e80d2d82cd1869e9a0b69d
Missael Gastelum <missaga@proton.me>
2025-01-23 14:40:47 -0700

    simplify transient prompt

--- .config/zsh/conf.d/zy_starship.zsh
+++ .config/zsh/conf.d/zy_starship.zsh
@@ -1,34 +1,24 @@
 if [[ -o interactive ]]; then
     eval "$(starship init zsh)"
-    _STARSHIP_EXE="$(which starship)"
 
-    zle-line-init() {
-        emulate -L zsh
+    TRANSIENT_PROMPT="${PROMPT// prompt / prompt --profile transient }"
+    TRANSIENT_RPROMPT="${PROMPT// prompt / prompt --profile rtransient }"
 
-        [[ $CONTEXT == start ]] || return 0
-
-        while true; do
-            zle .recursive-edit
-            local -i ret=$?
-            [[ $ret == 0 && $KEYS == $'\4' ]] || break
-            [[ -o ignore_eof ]] || exit 0
-        done
-
-        local saved_prompt=$PROMPT
-        local saved_rprompt=$RPROMPT
-        PROMPT='$('$_STARSHIP_EXE' prompt --profile transient --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-        RPROMPT='$('$_STARSHIP_EXE' prompt --profile rtransient --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-        zle .reset-prompt
-        PROMPT=$saved_prompt
-        RPROMPT="$saved_rprompt"
-
-        if (( ret )); then
-            zle .send-break
-        else
-            zle .accept-line
-        fi
-        return ret
+    zle -N send-break transient-prompt-send-break
+    function transient-prompt-send-break {
+        transient-prompt-zle-line-finish
+        zle .send-break
     }
 
-    zle -N zle-line-init
+    zle -N zle-line-finish transient-prompt-zle-line-finish
+    function transient-prompt-zle-line-finish {
+        PROMPT="$TRANSIENT_PROMPT" RPROMPT="$TRANSIENT_RPROMPT" zle reset-prompt
+    }
+
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd transient-prompt-precmd
+
+    function transient-prompt-precmd {
+        TRAPINT() { zle && transient-prompt-zle-line-finish; return $(( 128 + $1 )) }
+    }
 fi
