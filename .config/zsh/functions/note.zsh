aae885ee8c6d2c4e5e9f4c9fbffb186ef4d9b979
Missael Gastelum <missaga@proton.me>
2025-03-15 13:51:49 -0700

    fix obsidian loading

--- .config/zsh/functions/note.zsh
+++ .config/zsh/functions/note.zsh
@@ -1,6 +1,10 @@
 function note() {
     command -v nvim &>/dev/null || { echo neovim is not installed; return 1 }
     local vault="$HOME/Nextcloud/Apps/Obsidian/Personal/"
-
-    nvim "$vault" +'cd %:h | lua vim.defer_fn(function() vim.cmd("Lazy load obsidian.nvim") vim.cmd("set ma") end, 50)'
+    local file="$(find "$vault" -type f -print -quit)"
+    if [[ -n "$file" ]]; then
+        nvim "$file" +"cd $vault | bd | Dashboard"
+    else
+        nvim +"cd $vault"
+    fi
 }
