340e74e856130b11f5a9d3a8516309124d12c471
Missael Gastelum <missaga@proton.me>
2025-06-19 08:19:07 -0700

    fix rust path

--- .config/zsh/conf.d/rust.zsh
+++ .config/zsh/conf.d/rust.zsh
@@ -1,4 +1,4 @@
 local RUST_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/rust"
 export RUSTUP_HOME="$RUST_HOME/rustup"
 export CARGO_HOME="$RUST_HOME/cargo"
-[[ -f "$RUST_HOME/cargo/env" ]] && source "$RUST_HOME/cargo/env"
+path=("$RUST_HOME/cargo/bin" "${path[@]}")
