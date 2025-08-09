e3783faffa1502e0024e7e16e9cd1c83c460f457
Missael Gastelum <missaga@proton.me>
2025-08-09 16:03:27 -0700

    fix ctrl+c and time on transient

--- .config/zsh/conf.d/starship.zsh
+++ .config/zsh/conf.d/starship.zsh
@@ -3,18 +3,24 @@ if [[ -o interactive ]] && which starship &>/dev/null; then
 
     TRANSIENT_PROMPT="${PROMPT// prompt / prompt --profile transient }"
     TRANSIENT_RPROMPT="${PROMPT// prompt / prompt --profile rtransient }"
+    TRANSIENT_RPROMPT="${TRANSIENT_RPROMPT%%)} | sed -E \"s/([0-9]{2}:){2}[0-9]{2}/\$SAVED_TIME/\")" # Fix transient time
 
     autoload -Uz add-zle-hook-widget
+    add-zle-hook-widget zle-line-init transient-prompt-startup
     add-zle-hook-widget zle-line-finish transient-prompt
 
-    function transient-prompt() {
+    function transient-prompt-startup() { # Fix transient time
+        SAVED_TIME="$(date +%H:%M:%S)"
+    }
+
+    function transient-prompt() { # Use transient instead of default starship
         PROMPT="$TRANSIENT_PROMPT" RPROMPT="$TRANSIENT_RPROMPT" zle .reset-prompt
     }
 
-    # autoload -Uz add-zsh-hook
-    # add-zsh-hook precmd transient-prompt-precmd
-    #
-    # function transient-prompt-precmd {
-    #     TRAPINT() { transient-prompt; return $(( 128 + $1 )) }
-    # }
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd transient-prompt-precmd
+
+    function transient-prompt-precmd { # Fix ctrl+c behavior
+        TRAPINT() { transient-prompt; return $(( 128 + $1 )) }
+    }
 fi
