b484f7e29f9cbe0560cbf1dc7713d6a1bf1db60f
Missael Gastelum <missaga@proton.me>
2025-12-28 13:01:03 -0700

    better transient prompt

--- .config/zsh/conf.d/starship.zsh
+++ .config/zsh/conf.d/starship.zsh
@@ -1,38 +1,38 @@
-if [[ -o interactive ]] && which starship &>/dev/null; then
+if [[ -o interactive ]] && command -v starship &>/dev/null; then
     eval "$(starship init zsh)"
 
-    autoload -Uz add-zsh-hook
-    add-zsh-hook precmd transient-prompt-precmd
+    STARSHIP_TRANSIENT_PROMPT="${${PROMPT[@]:2:-1}// prompt / prompt --profile transient }"
+    STARSHIP_TRANSIENT_RPROMPT="${${PROMPT[@]:2:-1}// prompt / prompt --profile rtransient }"
 
-    TRANSIENT_PROMPT="${PROMPT// prompt / prompt --profile transient }"
-    TRANSIENT_RPROMPT="${PROMPT// prompt / prompt --profile rtransient }"
-
-    function transient-prompt-precmd {
-        # Fix ctrl+c behavior
-        TRAPINT() { transient-prompt 2>/dev/null; return $(( 128 + $1 )) }
-
+    function starship-transient-prompt-precmd {
         # Save transient prompt
-        SAVED_PROMPT="$(eval "printf '%s' \"${TRANSIENT_PROMPT}\"")"
-        SAVED_RPROMPT="$(eval "printf '%s' \"${TRANSIENT_RPROMPT}\"")"
+        STARSHIP_SAVED_PROMPT="$(eval "${STARSHIP_TRANSIENT_PROMPT}")"
+        STARSHIP_SAVED_RPROMPT="$(eval "${STARSHIP_TRANSIENT_RPROMPT}")"
+
+        # Fix ctrl+c behavior
+        TRAPINT() { starship-transient-prompt 2>/dev/null; return $(( 128 + $1 )) }
     }
 
-    autoload -Uz add-zle-hook-widget
-    add-zle-hook-widget zle-line-finish transient-prompt
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd starship-transient-prompt-precmd
 
-    function transient-prompt() {
+    function starship-transient-prompt() {
         # Use same directory transient prompt only
-        if [[ "$PWD" == "$SAVED_PROMPT_PWD" ]]; then
+        if [[ "$PWD" == "$STARSHIP_SAVED_PROMPT_PWD" ]]; then
             # Use saved transient prompt
-            PROMPT="$SAVED_PROMPT" RPROMPT="$SAVED_RPROMPT" zle .reset-prompt
+            PROMPT="$STARSHIP_SAVED_PROMPT" RPROMPT="$STARSHIP_SAVED_RPROMPT" zle .reset-prompt
         else
             # Save current directory to enable transient prompt next time
-            SAVED_PROMPT_PWD="$PWD"
+            STARSHIP_SAVED_PROMPT_PWD="$PWD"
         fi
     }
 
+    autoload -Uz add-zle-hook-widget
+    add-zle-hook-widget zle-line-finish starship-transient-prompt
+
     function clear-screen() {
         # Save transient prompt before clearing screen
-        transient-prompt-precmd
+        starship-transient-prompt-precmd
         zle .clear-screen
     }
 
